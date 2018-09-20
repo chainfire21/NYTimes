51e047906887d210eb1295f8feeee83166718fd8
aale12 <leanthony104@yahoo.com>
2018-09-19 20:33:43 -0500

    submit button function

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,14 +1,22 @@
 var searchQuery;
 var startDate;
 var endDate;
+var recordNum;
 
     $("#submit").on("click", function(){
-        
+
+        searchQuery = $("#search-term").val();
+        startDate = $("#s-year").val();
+        endDate = $("#e-year").val();
+        recordNum = $("#records").val();
+
         var url = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
         url += '?' + $.param({
             'api-key': "77b2df4c2d3244018dd444947fef304f",
-            'q': searchQUery,
-            
+            'q': searchQuery,
+            'begin_date': startDate,
+            'end_date': endDate,
+            'page': recordNum
         });
         
         $.ajax({ url: url, method: "GET" }).then(function (res) {
